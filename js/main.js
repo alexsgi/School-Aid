8b094092d5bd2fcbfeca905adc87a6be9829f20a
Alex <sagorski.entwicklung@gmail.com>
2023-10-18 12:33:20 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// JavaScript, um auf "Login" zu klicken und zu home.html weiterzuleiten
+document.getElementById('loginButton').addEventListener('click', function() {
+  // Hier können Sie die Weiterleitungs-URL anpassen
+  window.location.href = 'home.html';
+});
